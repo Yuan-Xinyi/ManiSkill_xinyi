f70d2011f2fd1e837748627cc57904423dcb32bd
Xinyi YUAN <xyuan749@connect.hkust-gz.edu.cn>
2025-12-04 17:30:46 +0900

    [model] now can generate path, detailed issues see daily report

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 python "$SCRIPT_DIR/examples/baselines/ppo/ppo.py" \
-    --env_id PickCube-v1 \
+    --env_id DrawStraightLine \
     --num_envs 1024 \
     --update_epochs 8 \
     --num_minibatches 32 \
