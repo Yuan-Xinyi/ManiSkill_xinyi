71394ff4e91b2b30173a6761a292441638f1542f
Xinyi YUAN <xyuan749@connect.hkust-gz.edu.cn>
2025-12-11 14:58:11 +0900

    [checkpoint eg5]

--- evaluate.sh
+++ evaluate.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 python "$SCRIPT_DIR/examples/baselines/ppo/ppo_evaluate.py" \
     --env_id DrawStraightLine \
-    --checkpoint runs/eg4_DrawStraightLine__ppo__1__1765266552/ckpt_1001.pt \
+    --checkpoint runs/DrawStraightLine__ppo__1__1765429842/ckpt_1001.pt \
     --capture-video
